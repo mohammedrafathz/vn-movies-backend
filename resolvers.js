59fab7f7671a7ac2a170ccc1824faf9a8e202104
mohammedrafathz <mohammedrafathz@gmail.com>
2022-12-15 18:34:11 -0500

    implemented vehicles crud

--- resolvers.js
+++ resolvers.js
@@ -2,11 +2,28 @@ const Vehicle = require('./models/Vehicle');
 
 const resolvers = {
   Query: {
-    hello: () => {
-      return "Hello World"
+    getAllVehicles: async () => {
+      return await Vehicle.find(); ƒ
     },
-    getAllVehicles: async () => { 
-      return await Vehicle.find();ƒ
+    getVehicleById: async (parent, {id}, context, info) => {
+      return await Vehicle.findById(id);
+    }
+  },
+  Mutation: {
+    addNewVehicle: async (parent, {vehicle}, context, info) => {
+      const veh = await new Vehicle({...vehicle}).save();
+
+      return veh
+    },
+    updateVehicle: async (parent, {id, vehicle}, context, info) => {
+      const veh = await Vehicle.findByIdAndUpdate(id, vehicle, {new: true})
+
+      return veh;
+    },
+    deleteVehicle: async (parent, {id}, context, info) => {
+      await Vehicle.findByIdAndDelete(id);
+
+      return "Successfully deleted vehicle";
     }
   }
 }
--- typeDefs.js
+++ typeDefs.js
@@ -14,6 +14,22 @@ const typeDefs = gql`
   type Query {
     hello: String
     getAllVehicles: [Vehicle]
+    getVehicleById(id: String): Vehicle
+  }
+
+  input VehicleInput {
+    image: String
+    make: String
+    vehicleModel: String
+    year: Int  
+    price: Int
+    status: Boolean
+  }
+
+  type Mutation {
+    addNewVehicle(vehicle: VehicleInput): Vehicle
+    updateVehicle(id: String, vehicle: VehicleInput): Vehicle
+    deleteVehicle(id: String): String
   }
 `
 
